2ef08ff54b1cec205fbdee4eebb0f51b3103f59d
alexmagno6m <alexacevedog@gmail.com>
2021-10-21 19:15:00 -0500

    conditionals

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 df = pd.read_csv(r'data.csv', encoding='ISO-8859-1', delimiter=',')
 df1 = df[['Age', 'Nationality']]
-a= df.describe()
-print(a)
+a= df[df ['Age'] < 25]
+print(a.head())
